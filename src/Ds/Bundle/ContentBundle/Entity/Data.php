25c138c72919b347be52c78791c13a234ee53319
mario <mario@digitalstate.ca>
2017-06-28 08:21:40 -0400

    Add entity, service and search filters

--- src/Ds/Bundle/ContentBundle/Entity/Data.php
+++ src/Ds/Bundle/ContentBundle/Entity/Data.php
@@ -2,10 +2,144 @@
 
 namespace Ds\Bundle\ContentBundle\Entity;
 
+use Ds\Component\Model\Type\Identifiable;
+use Ds\Component\Model\Type\Uuidentifiable;
+use Ds\Component\Model\Type\Translatable;
+use Ds\Component\Model\Type\Ownable;
+use Ds\Component\Model\Type\Versionable;
+use Ds\Component\Model\Attribute\Accessor;
+use Knp\DoctrineBehaviors\Model as Behavior;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use Symfony\Component\Serializer\Annotation as Serializer;
+use Symfony\Component\Validator\Constraints as Assert;
+use Ds\Component\Model\Annotation\Translate;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints as ORMAssert;
+
 /**
  * Class Data
+ *
+ * @ApiResource(
+ *      attributes={
+ *          "filters"={"ds.data.search", "ds.data.search_translation", "ds.data.date", "ds.data.order"},
+ *          "normalization_context"={
+ *              "groups"={"data_output"}
+ *          },
+ *          "denormalization_context"={
+ *              "groups"={"data_input"}
+ *          }
+ *      }
+ * )
+ * @ORM\Entity(repositoryClass="Ds\Bundle\ContentBundle\Repository\DataRepository")
+ * @ORM\Table(name="ds_data")
+ * @ORM\HasLifecycleCallbacks
+ * @ORMAssert\UniqueEntity(fields="uuid")
  */
-class Data
+class Data implements Identifiable, Uuidentifiable, Ownable, Translatable, Versionable
 {
+    use Behavior\Translatable\Translatable;
+    use Behavior\Timestampable\Timestampable;
+    use Behavior\SoftDeletable\SoftDeletable;
 
+    use Accessor\Id;
+    use Accessor\Uuid;
+    use Accessor\Owner;
+    use Accessor\OwnerUuid;
+    use Accessor\Translation\Title;
+    use Accessor\Version;
+
+    /**
+     * @var integer
+     * @ApiProperty(identifier=false, writable=false)
+     * @Serializer\Groups({"data_output"})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(name="id", type="integer")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ApiProperty(identifier=true, writable=false)
+     * @Serializer\Groups({"data_output"})
+     * @ORM\Column(name="uuid", type="guid", unique=true)
+     * @Assert\Uuid
+     */
+    protected $uuid;
+
+    /**
+     * @var \DateTime
+     * @ApiProperty(writable=false)
+     * @Serializer\Groups({"data_output"})
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ApiProperty(writable=false)
+     * @Serializer\Groups({"data_output"})
+     */
+    protected $updatedAt;
+
+    /**
+     * @var \DateTime
+     * @ApiProperty(writable=false)
+     * @Serializer\Groups({"data_output"})
+     */
+    protected $deletedAt;
+
+    /**
+     * @var string
+     * @ApiProperty
+     * @Serializer\Groups({"data_output", "data_input"})
+     * @ORM\Column(name="`owner`", type="string", length=255, nullable=true)
+     * @Assert\NotBlank
+     * @Assert\Length(min=1, max=255)
+     */
+    protected $owner;
+
+    /**
+     * @var string
+     * @ApiProperty
+     * @Serializer\Groups({"data_output", "data_input"})
+     * @ORM\Column(name="owner_uuid", type="guid", nullable=true)
+     * @Assert\NotBlank
+     * @Assert\Uuid
+     */
+    protected $ownerUuid;
+
+    /**
+     * @var array
+     * @ApiProperty
+     * @Serializer\Groups({"data_output", "data_input"})
+     * @Assert\Type("array")
+     * @Assert\NotBlank
+     * @Assert\All({
+     *     @Assert\NotBlank,
+     *     @Assert\Length(min=1)
+     * })
+     * @Translate
+     */
+    protected $title;
+
+    /**
+     * @var integer
+     * @ApiProperty
+     * @Serializer\Groups({"data_output", "data_input"})
+     * @ORM\Column(name="version", type="integer")
+     * @ORM\Version
+     * @Assert\NotBlank
+     * @Assert\Type("integer")
+     */
+    protected $version;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->title = [];
+    }
 }
---
+++ src/Ds/Bundle/ContentBundle/Entity/DataTranslation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ds\Bundle\ContentBundle\Entity;
+
+use Ds\Component\Model\Attribute\Accessor;
+use Knp\DoctrineBehaviors\Model as Behavior;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class DataTranslation
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="ds_data_trans")
+ */
+class DataTranslation
+{
+    use Behavior\Translatable\Translation;
+    use Behavior\Timestampable\Timestampable;
+    use Behavior\SoftDeletable\SoftDeletable;
+
+    use Accessor\Title;
+
+    /**
+     * @var string
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    protected $title;
+}
