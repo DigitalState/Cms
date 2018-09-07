761bf162e5ae3514b75d29ebfd451949e5120683
mario-digitalstate <mario@digitalstate.ca>
2018-09-07 17:10:35 -0400

    Refactor tenant loaders and unloaders

---
+++ src/AppBundle/Tenant/Unloader/FileUnloader.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Tenant\Unloader;
+
+use AppBundle\Entity\File;
+use Doctrine\ORM\EntityManager;
+use Ds\Component\Tenant\Entity\Tenant;
+use Ds\Component\Tenant\Loader\Unloader;
+
+/**
+ * Class FileUnloader
+ */
+class FileUnloader implements Unloader
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * Constructor
+     *
+     * @param \Doctrine\ORM\EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unload(Tenant $tenant)
+    {
+        $builder = $this->entityManager->getRepository(File::class)->createQueryBuilder('e');
+        $builder
+            ->delete()
+            ->where('e.tenant = :tenant')
+            ->setParameter('tenant', $tenant->getUuid());
+        $query = $builder->getQuery();
+        $query->execute();
+    }
+}
