cd7216737f820f707e02cb22443e8a1f987e1a11
mario-digitalstate <mario@digitalstate.ca>
2018-10-03 13:33:00 -0400

    Refactor based on new core

--- src/AppBundle/Migrations/Version1_0_0.php
+++ src/AppBundle/Migrations/Version1_0_0.php
@@ -29,7 +29,7 @@ class Version1_0_0 extends AbstractMigration implements ContainerAwareInterface
 
         switch ($platform) {
             case 'postgresql':
-                // Entity schema
+                // Schema
                 $this->addSql('CREATE SEQUENCE ds_config_id_seq INCREMENT BY 1 MINVALUE 1 START 9');
                 $this->addSql('CREATE SEQUENCE ds_parameter_id_seq INCREMENT BY 1 MINVALUE 1 START 4');
                 $this->addSql('CREATE SEQUENCE ds_metadata_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
@@ -45,10 +45,10 @@ class Version1_0_0 extends AbstractMigration implements ContainerAwareInterface
                 $this->addSql('CREATE SEQUENCE app_page_trans_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
                 $this->addSql('CREATE SEQUENCE app_text_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
                 $this->addSql('CREATE SEQUENCE app_text_trans_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-                $this->addSql('CREATE TABLE ds_config (id INT NOT NULL, uuid UUID NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, "key" VARCHAR(255) NOT NULL, value JSON DEFAULT NULL, encrypted BOOLEAN NOT NULL, enabled BOOLEAN NOT NULL, version INT DEFAULT 1 NOT NULL, tenant UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+                $this->addSql('CREATE TABLE ds_config (id INT NOT NULL, uuid UUID NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, "key" VARCHAR(255) NOT NULL, value TEXT DEFAULT NULL, version INT DEFAULT 1 NOT NULL, tenant UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
                 $this->addSql('CREATE UNIQUE INDEX UNIQ_758C45F4D17F50A6 ON ds_config (uuid)');
                 $this->addSql('CREATE UNIQUE INDEX UNIQ_758C45F48A90ABA94E59C462 ON ds_config (key, tenant)');
-                $this->addSql('CREATE TABLE ds_parameter (id INT NOT NULL, "key" VARCHAR(255) NOT NULL, value JSON DEFAULT NULL, encrypted BOOLEAN NOT NULL, enabled BOOLEAN NOT NULL, PRIMARY KEY(id))');
+                $this->addSql('CREATE TABLE ds_parameter (id INT NOT NULL, "key" VARCHAR(255) NOT NULL, value TEXT DEFAULT NULL, PRIMARY KEY(id))');
                 $this->addSql('CREATE UNIQUE INDEX UNIQ_B3C0FD91F48571EB ON ds_parameter ("key")');
                 $this->addSql('CREATE TABLE ds_metadata (id INT NOT NULL, uuid UUID NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, slug VARCHAR(255) NOT NULL, type VARCHAR(255) DEFAULT NULL, data JSON NOT NULL, version INT DEFAULT 1 NOT NULL, tenant UUID NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
                 $this->addSql('CREATE UNIQUE INDEX UNIQ_11290F17D17F50A6 ON ds_metadata (uuid)');
@@ -92,63 +92,181 @@ class Version1_0_0 extends AbstractMigration implements ContainerAwareInterface
                 $this->addSql('ALTER TABLE app_file_trans ADD CONSTRAINT FK_8CB905F62C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES app_file (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
                 $this->addSql('ALTER TABLE app_page_trans ADD CONSTRAINT FK_40BB4A172C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES app_page (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
                 $this->addSql('ALTER TABLE app_text_trans ADD CONSTRAINT FK_708E0F9C2C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES app_text (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-
-                // Custom schema
                 $this->addSql('CREATE TABLE ds_session (id VARCHAR(128) NOT NULL PRIMARY KEY, data BYTEA NOT NULL, time INTEGER NOT NULL, lifetime INTEGER NOT NULL)');
 
                 // Data
                 $yml = file_get_contents('/srv/api-platform/src/AppBundle/Resources/migrations/1_0_0.yml');
                 $data = Yaml::parse($yml);
+                $i = 0;
+                $parameters = [
+                    [
+                        'key' => 'ds_system.user.username',
+                        'value' => serialize($data['system']['username'])
+                    ],
+                    [
+                        'key' => 'ds_system.user.password',
+                        'value' => $cipherService->encrypt($data['system']['password'])
+                    ],
+                    [
+                        'key' => 'ds_tenant.tenant.default',
+                        'value' => serialize($data['tenant']['uuid'])
+                    ]
+                ];
 
-                $this->addSql('
-                    INSERT INTO 
-                        ds_parameter (id, key, value, encrypted, enabled)
-                    VALUES 
-                        (1, \'ds_system.user.username\', \'"'.$data['system']['username'].'"\', false, true),
-                        (2, \'ds_system.user.password\', \'"'.$cipherService->encrypt($data['system']['password']).'"\', true, true),
-                        (3, \'ds_tenant.tenant.default\', \'"'.$data['tenant']['uuid'].'"\', false, true);
-                ');
+                foreach ($parameters as $parameter) {
+                    $this->addSql(sprintf(
+                        'INSERT INTO ds_parameter (id, key, value) VALUES (%d, %s, %s);',
+                        ++$i,
+                        $this->connection->quote($parameter['key']),
+                        $this->connection->quote($parameter['value'])
+                    ));
+                }
 
-                $this->addSql('
-                    INSERT INTO 
-                        ds_tenant (id, uuid, data, created_at, updated_at)
-                    VALUES 
-                        (1, \''.$data['tenant']['uuid'].'\', \'"'.$cipherService->encrypt(new stdClass).'"\', now(), now());
-                ');
+                $i = 0;
+                $tenants = [
+                    [
+                        'uuid' => $data['tenant']['uuid'],
+                        'data' => '"'.$cipherService->encrypt(new stdClass).'"'
+                    ]
+                ];
 
-                $this->addSql('
-                    INSERT INTO 
-                        ds_config (id, uuid, owner, owner_uuid, key, value, encrypted, enabled, version, tenant, created_at, updated_at)
-                    VALUES 
-                        (1, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.username\', \'"'.$data['user']['system']['username'].'"\', false, true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
-                        (2, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.password\', \'"'.$cipherService->encrypt($data['user']['system']['password']).'"\', true, true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
-                        (3, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.uuid\', \'"'.$data['user']['system']['uuid'].'"\', false, true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
-                        (4, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.roles\', \'[]\', false, true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
-                        (5, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.identity.roles\', \'[]\', false, true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
-                        (6, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.identity.type\', \'"System"\', false, true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
-                        (7, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.identity.uuid\', \'"'.$data['identity']['system']['uuid'].'"\', false, true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
-                        (8, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.tenant\', \'"'.$data['tenant']['uuid'].'"\', false, true, 1, \''.$data['tenant']['uuid'].'\', now(), now());
-                ');
+                foreach ($tenants as $tenant) {
+                    $this->addSql(sprintf(
+                        'INSERT INTO ds_tenant (id, uuid, data, created_at, updated_at) VALUES (%d, %s, %s, %s, %s);',
+                        ++$i,
+                        $this->connection->quote($tenant['uuid']),
+                        $this->connection->quote($tenant['data']),
+                        'now()',
+                        'now()'
+                    ));
+                }
 
-                $this->addSql('
-                    INSERT INTO 
-                        ds_access (id, uuid, owner, owner_uuid, assignee, assignee_uuid, version, tenant, created_at, updated_at)
-                    VALUES 
-                        (1, \''.Uuid::uuid4()->toString().'\', \'System\', \''.$data['identity']['system']['uuid'].'\', \'System\', \''.$data['identity']['system']['uuid'].'\', 1, \''.$data['tenant']['uuid'].'\', now(), now()),
-                        (2, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'Staff\', \''.$data['identity']['admin']['uuid'].'\', 1, \''.$data['tenant']['uuid'].'\', now(), now());
-                ');
+                $i = 0;
+                $configs = [
+                    [
+                        'key' => 'ds_api.user.username',
+                        'value' => serialize($data['user']['system']['username'])
+                    ],
+                    [
+                        'key' => 'ds_api.user.password',
+                        'value' => $cipherService->encrypt($data['user']['system']['password'])
+                    ],
+                    [
+                        'key' => 'ds_api.user.uuid',
+                        'value' => serialize($data['user']['system']['uuid'])
+                    ],
+                    [
+                        'key' => 'ds_api.user.roles',
+                        'value' => serialize([])
+                    ],
+                    [
+                        'key' => 'ds_api.user.identity.roles',
+                        'value' => serialize([])
+                    ],
+                    [
+                        'key' => 'ds_api.user.identity.type',
+                        'value' => serialize('System')
+                    ],
+                    [
+                        'key' => 'ds_api.user.identity.uuid',
+                        'value' => serialize($data['identity']['system']['uuid'])
+                    ],
+                    [
+                        'key' => 'ds_api.user.tenant',
+                        'value' => serialize($data['tenant']['uuid'])
+                    ]
+                ];
 
-                $this->addSql('
-                    INSERT INTO 
-                        ds_access_permission (id, access_id, scope, entity, entity_uuid, key, attributes, tenant)
-                    VALUES 
-                        (1, 1, \'generic\', NULL, NULL, \'entity\', \'["BROWSE","READ","EDIT","ADD","DELETE"]\', \''.$data['tenant']['uuid'].'\'),
-                        (2, 1, \'generic\', NULL, NULL, \'property\', \'["BROWSE","READ","EDIT"]\', \''.$data['tenant']['uuid'].'\'),
-                        (3, 1, \'generic\', NULL, NULL, \'generic\', \'["BROWSE","READ","EDIT","ADD","DELETE","EXECUTE"]\', \''.$data['tenant']['uuid'].'\'),
-                        (4, 2, \'generic\', NULL, NULL, \'entity\', \'["BROWSE","READ","EDIT","ADD","DELETE"]\', \''.$data['tenant']['uuid'].'\'),
-                        (5, 2, \'generic\', NULL, NULL, \'property\', \'["BROWSE","READ","EDIT"]\', \''.$data['tenant']['uuid'].'\'),
-                        (6, 2, \'generic\', NULL, NULL, \'generic\', \'["BROWSE","READ","EDIT","ADD","DELETE","EXECUTE"]\', \''.$data['tenant']['uuid'].'\');
-                ');
+                foreach ($configs as $config) {
+                    $this->addSql(sprintf(
+                        'INSERT INTO ds_config (id, uuid, owner, owner_uuid, key, value, version, tenant, created_at, updated_at) VALUES (%d, %s, %s, %s, %s, %s, %d, %s, %s, %s);',
+                        ++$i,
+                        $this->connection->quote(Uuid::uuid4()->toString()),
+                        $this->connection->quote('BusinessUnit'),
+                        $this->connection->quote($data['business_unit']['administration']['uuid']),
+                        $this->connection->quote($config['key']),
+                        $this->connection->quote($config['value']),
+                        1,
+                        $this->connection->quote($data['tenant']['uuid']),
+                        'now()',
+                        'now()'
+                    ));
+                }
+
+                $i = 0;
+                $j = 0;
+                $accesses = [
+                    [
+                        'owner' => 'System',
+                        'owner_uuid' => $data['identity']['system']['uuid'],
+                        'assignee' => 'System',
+                        'assignee_uuid' => $data['identity']['system']['uuid'],
+                        'permissions' => [
+                            [
+                                'key' => 'entity',
+                                'attributes' => '["BROWSE","READ","EDIT","ADD","DELETE"]'
+                            ],
+                            [
+                                'key' => 'property',
+                                'attributes' => '["BROWSE","READ","EDIT"]'
+                            ],
+                            [
+                                'key' => 'generic',
+                                'attributes' => '["BROWSE","READ","EDIT","ADD","DELETE","EXECUTE"]'
+                            ]
+                        ]
+                    ],
+                    [
+                        'owner' => 'BusinessUnit',
+                        'owner_uuid' => $data['business_unit']['administration']['uuid'],
+                        'assignee' => 'Staff',
+                        'assignee_uuid' => $data['identity']['admin']['uuid'],
+                        'permissions' => [
+                            [
+                                'key' => 'entity',
+                                'attributes' => '["BROWSE","READ","EDIT","ADD","DELETE"]'
+                            ],
+                            [
+                                'key' => 'property',
+                                'attributes' => '["BROWSE","READ","EDIT"]'
+                            ],
+                            [
+                                'key' => 'generic',
+                                'attributes' => '["BROWSE","READ","EDIT","ADD","DELETE","EXECUTE"]'
+                            ]
+                        ]
+                    ]
+                ];
+
+                foreach ($accesses as $access) {
+                    $this->addSql(sprintf(
+                        'INSERT INTO ds_access (id, uuid, owner, owner_uuid, assignee, assignee_uuid, version, tenant, created_at, updated_at) VALUES (%d, %s, %s, %s, %s, %s, %d, %s, %s, %s);',
+                        ++$i,
+                        $this->connection->quote(Uuid::uuid4()->toString()),
+                        $this->connection->quote($access['owner']),
+                        $this->connection->quote($access['owner_uuid']),
+                        $this->connection->quote($access['assignee']),
+                        $this->connection->quote($access['assignee_uuid']),
+                        1,
+                        $this->connection->quote($data['tenant']['uuid']),
+                        'now()',
+                        'now()'
+                    ));
+
+                    foreach ($access['permissions'] as $permission) {
+                        $this->addSql(sprintf(
+                            'INSERT INTO ds_access_permission (id, access_id, scope, entity, entity_uuid, key, attributes, tenant) VALUES (%d, %d, %s, %s, %s, %s, %s, %s);',
+                            ++$j,
+                            $i,
+                            $this->connection->quote('generic'),
+                            'NULL',
+                            'NULL',
+                            $this->connection->quote($permission['key']),
+                            $this->connection->quote($permission['attributes']),
+                            $this->connection->quote($data['tenant']['uuid'])
+                        ));
+                    }
+                }
 
                 break;
 
@@ -169,7 +287,7 @@ class Version1_0_0 extends AbstractMigration implements ContainerAwareInterface
 
         switch ($platform) {
             case 'postgresql':
-                // Entity schema
+                // Schema
                 $this->addSql('ALTER TABLE ds_metadata_trans DROP CONSTRAINT FK_A6447E202C2AC5D3');
                 $this->addSql('ALTER TABLE ds_access_permission DROP CONSTRAINT FK_D46DD4D04FEA67CF');
                 $this->addSql('ALTER TABLE app_data_trans DROP CONSTRAINT FK_6885795E2C2AC5D3');
@@ -206,8 +324,6 @@ class Version1_0_0 extends AbstractMigration implements ContainerAwareInterface
                 $this->addSql('DROP TABLE app_page_trans');
                 $this->addSql('DROP TABLE app_text');
                 $this->addSql('DROP TABLE app_text_trans');
-
-                // Custom schema
                 $this->addSql('DROP TABLE ds_session');
                 break;
 
